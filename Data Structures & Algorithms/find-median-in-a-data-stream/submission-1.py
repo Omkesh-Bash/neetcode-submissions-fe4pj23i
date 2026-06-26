2a2c4c3a8d4ecaf121c84f2b3667788791a2662e
Omkesh Tidke <omkeshtidke596@gmail.com>
2026-06-26 13:44:04 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.py
@@ -0,0 +1,35 @@
+class MedianFinder:
+
+    # def get_at_index(self, lst, index, default) -> float:
+    #     try:
+    #         return lst[index]
+    #     except IndexError:
+    #         return default
+
+    def __init__(self):
+        self.small_heap, self.large_heap = [], []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small_heap, -num)
+        # right elements
+        
+        if(self.large_heap and -self.small_heap[0] > self.large_heap[0]): # its ok if they are equal
+            heapq.heappush(self.large_heap, -heapq.heappop(self.small_heap))
+        
+        # balance
+        bf = len(self.small_heap) - len(self.large_heap)
+        if(bf > 1): # small_heap is bigger
+            heapq.heappush(self.large_heap, -heapq.heappop(self.small_heap))
+        elif(bf < -1): # large_heap is bigger
+            heapq.heappush(self.small_heap, -heapq.heappop(self.large_heap))
+
+
+    def findMedian(self) -> float:
+        # balance
+        bf = len(self.small_heap) - len(self.large_heap)
+        if(bf == 1): # small_heap is bigger
+            return -self.small_heap[0]
+        elif(bf == -1): # large_heap is bigger
+            return self.large_heap[0]
+        else:
+            return ((-self.small_heap[0]) + self.large_heap[0]) / 2
