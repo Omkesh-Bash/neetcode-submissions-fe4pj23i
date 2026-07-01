e14189e6ffb5bf6d472cd5e358d37784921f2227
Omkesh Tidke <omkeshtidke596@gmail.com>
2026-07-01 15:16:47 +0530

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        m, n = len(grid), len(grid[0])
+        directions = ((0, -1), (0, 1), (1, 0), (-1, 0))
+        def dfs(r : int, c : int, d : int):
+            if not (r >= 0 and r < m and c >= 0 and c < n and grid[r][c] != -1 and grid[r][c] > d ):
+                return
+            grid[r][c] = d
+            d+=1
+            for dr, dc in directions:
+                dfs(r + dr, c + dc, d)
+
+
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 0:
+                    for dr, dc in directions:
+                        dfs(r + dr, c + dc, 1)
