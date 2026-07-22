770ccbb524c074658ffad5a58a65d8afd793cee6
Omkesh Tidke <omkeshtidke596@gmail.com>
2026-07-22 17:37:14 +0530

    Add: min-cost-to-connect-points - submission-3

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-3.py
@@ -0,0 +1,50 @@
+
+class Disjoit_set:
+    def __init__(self, size : int):
+        self.parent = [i for i in range(size)]
+        self.size = [1]* (size + 1)
+    
+    def ultimate_parent(self, node : int) -> int:
+        if node == self.parent[node]:
+            return node
+        self.parent[node] = self.ultimate_parent(self.parent[node])
+        return self.parent[node]
+    
+    def union_by_size(self, u : int, v : int):
+        par_u, par_v = self.ultimate_parent(u), self.ultimate_parent(v)
+        if par_u == par_v: return False
+        if self.size[par_u] > self.size[par_v]:
+            self.parent[par_v] = par_u
+            self.size[par_u] += self.size[par_v]
+        else:
+            self.parent[par_u] = par_v
+            self.size[par_v] += self.size[par_u]
+        return True
+
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        X, Y = 0, 1
+        visited = [False]*len(points)
+        adj_list = [[] for _ in range(len(points))] # cost, destination_index
+
+        min_heap = []
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                cost = abs(points[i][X] - points[j][X]) + abs(points[i][Y] - points[j][Y])
+                # adj_list[i].append((cost, j))
+                # adj_list[j].append((cost, i))
+                min_heap.append((cost, i, j))
+
+        heapq.heapify(min_heap)
+        # min_heap = [(0, 0 ,0)] # cost, u, v
+        cost, size  = 0, 1
+        dset =  Disjoit_set(len(points))
+        while size != len(points):
+            m_cost, u, v = heapq.heappop(min_heap)
+            if not dset.union_by_size(u, v):
+                continue
+            cost += m_cost
+            size += 1
+        return cost
