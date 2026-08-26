49faa35df8f2c3426873d1f058dc0fbdea3bfc82
Omkesh Tidke <omkeshtidke596@gmail.com>
2026-08-26 16:59:50 +0530

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        n1, n2 = 1, 0
+        for i in range(n-1):
+            n1, n2 = n1 + n2, n1
+        return n1 + n2
